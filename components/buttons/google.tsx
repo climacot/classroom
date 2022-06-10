928595730f9a2c70f93e5f34a72be5efd2d8e3c0
climacot <fermt111@gmail.com>
2022-06-09 22:39:11 -0500

    propiedad que no debia estar en el componente

--- components/buttons/google.tsx
+++ components/buttons/google.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from "react";
 export default function ButtonGoogle({ children }: { children: ReactNode }) {
   return (
     <Link href={"/"}>
-      <a className="text-center text-lg p-3 text-white bg-blue-500 hover:bg-blue-600 duration-700 rounded-md mr-5">{children}</a>
+      <a className="grow text-center text-lg p-3 text-white bg-blue-500 hover:bg-blue-600 duration-700 rounded-md">{children}</a>
     </Link>
   );
 }
