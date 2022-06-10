ebe22a5029581143c1423c3f152fcc7a938910ce
climacot <fermt111@gmail.com>
2022-06-09 19:53:34 -0500

    logo de google basico

---
+++ components/logos/google.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import google from "../../public/google.svg";
+
+export default function Google() {
+  return (
+    <div className="flex items-center">
+      <Image src={google} alt="Logo Google" />
+    </div>
+  );
+}
