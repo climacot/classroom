e9c14efd6f32ecd68da987e405179ce2788c7d99
climacot <fermt111@gmail.com>
2022-06-10 15:10:21 -0500

    cambio de nombre para mejorar la semantica

--- components/buttons/google.tsx
+++ components/buttons/google.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import React, { ReactNode } from "react";
 
-export default function ButtonGoogle({ children }: { children: ReactNode }) {
+export default function LinkGoogle({ children }: { children: ReactNode }) {
   return (
     <Link href={"/"}>
-      <a className="grow text-center text-lg p-3 text-white bg-blue-500 hover:bg-blue-600 duration-700 rounded-md">{children}</a>
+      <a className="grow text-center text-lg p-3 text-white bg-blue-500 hover:bg-blue-600 duration-700 rounded-md">
+        {children}
+      </a>
     </Link>
   );
 }
