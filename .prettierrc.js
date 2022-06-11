6a9409772f1798e2ca5ecd46c0f92dbd71f20126
climacot <fermt111@gmail.com>
2022-06-11 08:17:42 -0500

    configuracion de linter

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid'
+}
