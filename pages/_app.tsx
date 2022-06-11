eefd4e903e6f2716ffe7543caf452fac3dee6094
climacot <fermt111@gmail.com>
2022-06-10 22:11:39 -0500

    la sesion ya no se controla por firebase sino por next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import AuthProvider from "../provider/AuthProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
       <Component {...pageProps} />
-    </AuthProvider>
+    </SessionProvider>
   );
 }
 
