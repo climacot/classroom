c701146a7e4378d6f62ca3d508a56bd7d843f294
climacot <fermt111@gmail.com>
2022-06-11 08:17:55 -0500

    refactor

--- components/buttons/signOut.tsx
+++ components/buttons/signOut.tsx
@@ -1,24 +1,17 @@
 import React, { ReactNode } from "react";
-import useAuth from "../../hooks/useAuth";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 type ComponentProps = {
   children: ReactNode;
 };
 
 export default function SignOutButtonGoogle({ children }: ComponentProps) {
-  const auth = useAuth();
-
   return (
-    <a
-      href={`/api/auth/signout`}
+    <button
       className="grow text-center text-lg p-3 text-white bg-blue-500 hover:bg-blue-600 duration-700 rounded-md"
-      onClick={(e) => {
-        e.preventDefault();
-        signOut();
-      }}
+      onClick={() => signOut()}
     >
       {children}
-    </a>
+    </button>
   );
 }
