e6cc4e1e7cd16ed8dd93c38efab256d3eae88ecb
climacot <fermt111@gmail.com>
2022-06-12 11:39:08 -0500

    menu para crear o unirse a una clase

---
+++ components/buttons/menu.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Image from 'next/image'
+import useModal from '../../hooks/useModal'
+
+export default function Menu() {
+  const [open, setOpen] = useState(false)
+  const { handleModalCreate } = useModal()
+
+  const handleClick = () => setOpen(!open)
+  const handleOpenModal = () => handleModalCreate()
+
+  return (
+    <>
+      <button onClick={handleClick} className='rounded-full p-2 flex items-center mr-3 hover:bg-gray-100'>
+        <Image className='pointer-events-none' src={'/plus.png'} width={34} height={34} />
+      </button>
+      <div className={`${open ? 'block' : 'hidden'} absolute right-0 top-14 mr-5 bg-white`}>
+        <div className='w-40 flex flex-col text-center rounded-md shadow-lg border pt-2 pb-2'>
+          <button onClick={handleOpenModal} className='p-3 hover:bg-gray-100'>
+            Unirme a clase
+          </button>
+          <button className='p-3 hover:bg-gray-100'>Crear clase</button>
+        </div>
+      </div>
+    </>
+  )
+}
