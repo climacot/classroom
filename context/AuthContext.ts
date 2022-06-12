8ade47c594f9487afa4f2c16a9d3059edb3138c7
climacot <fermt111@gmail.com>
2022-06-12 11:39:53 -0500

    deprecated

--- context/AuthContext.ts
+++ context/AuthContext.ts
@@ -1,27 +1,27 @@
-import { createContext } from "react";
+import { createContext } from 'react'
 
 export const userInit = {
   user: {
     displayName: undefined,
     email: undefined,
-    photoUrl: undefined,
+    photoUrl: undefined
   },
   userIsLoggedIn: false,
   login: () => {},
-  signOut: () => {},
-};
+  signOut: () => {}
+}
 
 export interface IuserContext {
-  user: Iuser | undefined;
-  userIsLoggedIn: boolean;
-  login: () => void;
-  signOut: () => void;
+  user: Iuser | undefined
+  userIsLoggedIn: boolean
+  login: () => void
+  signOut: () => void
 }
 
 export interface Iuser {
-  displayName: string | undefined | null;
-  email: string | undefined | null;
-  photoUrl: string | undefined | null;
+  displayName: string | undefined | null
+  email: string | undefined | null
+  photoUrl: string | undefined | null
 }
 
-export const AuthContext = createContext<IuserContext>(userInit);
+export const AuthContext = createContext<IuserContext>(userInit)
--- provider/AuthProvider.tsx
+++ provider/AuthProvider.tsx
@@ -1,51 +1,51 @@
-import { auth } from "../firebase";
-import { AuthContext, Iuser, userInit } from "../context/AuthContext";
-import { onAuthStateChanged } from "firebase/auth";
-import { ReactNode, useEffect, useState } from "react";
-import { signInWithPopupFirebase, signOutFirebase } from "../firebase/auth";
+import { auth } from '../firebase'
+import { AuthContext, Iuser, userInit } from '../context/AuthContext'
+import { onAuthStateChanged } from 'firebase/auth'
+import { ReactNode, useEffect, useState } from 'react'
+import { signInWithPopupFirebase, signOutFirebase } from '../firebase/auth'
 
 type ComponentProps = {
-  children: ReactNode;
-};
+  children: ReactNode
+}
 
 export default function AuthProvider({ children }: ComponentProps) {
-  const [user, setUser] = useState<Iuser | undefined>(undefined);
+  const [user, setUser] = useState<Iuser | undefined>(undefined)
 
   useEffect(() => {
     const unsuscribe = onAuthStateChanged(auth, function (user) {
       if (!user) {
-        setUser(undefined);
-        return;
+        setUser(undefined)
+        return
       }
 
       const decorateUser = {
         displayName: user?.displayName,
         email: user?.email,
-        photoUrl: user?.photoURL,
-      };
+        photoUrl: user?.photoURL
+      }
 
-      setUser(decorateUser);
-    });
+      setUser(decorateUser)
+    })
 
-    return () => unsuscribe();
-  }, []);
+    return () => unsuscribe()
+  }, [])
 
   const login = async () => {
-    const userLogin = await signInWithPopupFirebase();
+    const userLogin = await signInWithPopupFirebase()
     const decorateUser = {
       displayName: userLogin?.displayName,
       email: userLogin?.email,
-      photoUrl: userLogin?.photoURL,
-    };
-    setUser(decorateUser);
-    window.localStorage.setItem("session", JSON.stringify(decorateUser));
-  };
+      photoUrl: userLogin?.photoURL
+    }
+    setUser(decorateUser)
+    window.localStorage.setItem('session', JSON.stringify(decorateUser))
+  }
 
   const signOut = async () => {
-    await signOutFirebase();
-  };
+    await signOutFirebase()
+  }
 
-  const userIsLoggedIn = user ? true : false;
+  const userIsLoggedIn = user ? true : false
 
-  return <AuthContext.Provider value={{ user, login, signOut, userIsLoggedIn }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ user, login, signOut, userIsLoggedIn }}>{children}</AuthContext.Provider>
 }
