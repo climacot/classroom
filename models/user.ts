87e7334929cdfd59a6919f97c7a6d1e3fd207f7b
climacot <fermt111@gmail.com>
2022-06-17 15:45:35 -0500

    models

---
+++ models/user.ts
@@ -0,0 +1,13 @@
+export interface Iuser {
+  displayName: string
+  email: string
+  id: string
+  photoURL: string
+}
+
+export interface Icourse {
+  id: string
+  name: string
+  owner: string
+  timestamp: number
+}
