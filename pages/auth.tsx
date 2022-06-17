d60bbb274959d22e58453d40101cedee268f3adb
climacot <fermt111@gmail.com>
2022-06-17 15:45:45 -0500

    componente loader

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,16 +1,26 @@
 import { AuthAction, withAuthUser } from 'next-firebase-auth'
 import { signInWithPopupFirebase } from '../firebase/auth'
+import google from '../public/google_g.svg'
+import Image from 'next/image'
+import LoaderHeader from '../components/Loaders/LoaderHeader'
 
 const RedirectPage = () => {
   return (
-    <div>
-      <button onClick={signInWithPopupFirebase}>Iniciar con googlee</button>
+    <div className='absolute flex items-center justify-center h-full w-full'>
+      <button className='flex shadow-sm p-3 border rounded-md' onClick={signInWithPopupFirebase}>
+        <div className='w-8'>
+          <div className='flex items-center'>
+            <Image src={google} alt='Logo de google minimo' />
+          </div>
+        </div>
+        <span className='ml-3'>Continuar con Google</span>
+      </button>
     </div>
   )
 }
 
 export default withAuthUser({
   whenAuthed: AuthAction.REDIRECT_TO_APP,
-  whenUnauthedBeforeInit: AuthAction.RETURN_NULL,
-  whenUnauthedAfterInit: AuthAction.RENDER
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  LoaderComponent: LoaderHeader
 })(RedirectPage)
