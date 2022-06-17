56e4ee3437067e118d9627ca5e435b0c85353b9c
climacot <fermt111@gmail.com>
2022-06-17 15:45:16 -0500

    refactor

--- components/logos/google.tsx
+++ components/logos/google.tsx
@@ -3,8 +3,8 @@ import google from '../../public/google.svg'
 
 export default function Google() {
   return (
-    <div draggable='false' className='flex items-center'>
-      <Image className='pointer-events-none' src={google} alt='Logo Google' />
+    <div className='flex items-center'>
+      <Image src={google} alt='Logo Google' />
     </div>
   )
 }
