721d828de12073ce0ce0202fbd3bf1492a6a0f5a
climacot <fermt111@gmail.com>
2022-06-21 11:09:04 -0500

    configuracion inicial de next-firebase-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import initAuth from '../initAuth' // the module you created above
 import '../styles/global.css'
+
+initAuth()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
