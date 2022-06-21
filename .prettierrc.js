186d194b5fb955d4f907003a09d236d0aa36a90c
climacot <fermt111@gmail.com>
2022-06-21 10:41:24 -0500

    configuracion inicial de linter y tailwindcss

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 120,
+  arrowParens: "avoid"
+}
